237e10354515b4966a22cad6682f5e36e98fbd78
Marko9827 <marko.supergun@gmail.com>
2017-06-27 10:26:15 +0200

    erf

--- js/index.js
+++ js/index.js
@@ -65,3 +65,27 @@
         }, '-=.5');
     });
 })();
+
+
+
+
+
+ 
+document.onkeypress = function (event) {  
+event = (event || window.event);  
+if (event.keyCode == 123) {  
+return false;  
+}  
+}  
+document.onmousedown = function (event) {  
+event = (event || window.event);  
+if (event.keyCode == 123) {  
+return false;  
+}  
+}  
+document.onkeydown = function (event) {  
+event = (event || window.event);  
+if (event.keyCode == 123) {  
+return false;  
+}  
+}  
